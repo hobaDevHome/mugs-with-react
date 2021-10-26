f49e261b565f2a71fd92c81df7937386e181da12
hobaDevHome <heba96@yahoo.com>
2021-10-26 16:14:46 +0200

    cart added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
-import Filters from './comps/filters/Filters';
-import Products from './comps/products/Products';
+import React from "react";
+import Cart from "./comps/Cart/Cart";
+import Filters from "./comps/filters/Filters";
+import Products from "./comps/products/Products";
 
 function App() {
   return (
-    <div>
-      <Filters />
-      <Products />
+    <div className="filterDev">
+      <div className="filter">
+        <Filters />
+      </div>
+      <div className="mainDev">
+        <div className="left">
+          <Products />
+        </div>
+        <div className="right">
+          <Cart />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/comps/Cart/Cart.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Cart() {
+    return (
+        <div className="border-container cartContainer">
+           <h2 className="title">Cart</h2>
+        </div>
+    )
+}
+
+export default Cart
